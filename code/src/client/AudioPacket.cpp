74f718e7463f04ae455a4411c88eff5d019b2ce1
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2020-10-05 17:11:24 +0200

    [+] AudioPacket

---
+++ code/src/client/AudioPacket.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** Mathieu Schmerber
+** File description:
+** AudioPacket
+*/
+
+#include "AudioPacket.hpp"
+
+AudioPacket::AudioPacket()
+{
+
+}
+
+AudioPacket::~AudioPacket() {}

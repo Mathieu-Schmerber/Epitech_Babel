8f95c42fa614ddd2c4342cfc0777fae762d07645
thomas crombez <tcrombez@localhost.localdomain>
2020-10-08 11:51:31 -0400

    [+] SQL Database getContactQuery

--- code/src/server/SQLdatabase.cpp
+++ code/src/server/SQLdatabase.cpp
@@ -5,7 +5,9 @@
 ** babel
 */
 
+#include "TcpQuery.hpp"
 #include "SQLdatabase.hpp"
+#include "Contact.hpp"
 
 SQLdatabase::SQLdatabase()
 {
@@ -37,4 +39,43 @@ SQLdatabase::~SQLdatabase()
 int SQLdatabase::callback(void *not_used, int ac, char **av, char **azColName)
 {
     return(0);
+}
+
+int SQLdatabase::getContactList(void *data, int ac, char **av, char **azColName)
+{
+    std::string port;
+    std::string name;
+    std::string ip;
+
+    for (int i = 0; i < ac; i++) {
+        if (strcmp(azColName[i], "NAME") == 0)
+            name.assign(av[i]);
+        if (strcmp(azColName[i], "PORT") == 0)
+            port.assign(av[i]);
+        if (strcmp(azColName[i], "IP") == 0)
+            ip.assign(av[i]);
+        printf("%s = %s\n", azColName[i], av[i] ? av[i] : "NULL");
+    }
+    Contact contact(ip, name, std::stoi(port));
+    std::vector<Contact> *contactList = static_cast<std::vector<Contact>*>(data);
+    contactList->push_back(contact);
+    return 0;
+}
+
+std::string SQLdatabase::getContactQuery() {
+    TcpQuery query(TcpQuery::CONNECT);
+
+    if (query.getType() == TcpQuery::CONNECT) {
+        std::vector<Contact> contacts;
+        std::string sqlselect = "SELECT * from CONTACT";
+        rc = sqlite3_exec(db, sqlselect.c_str(), SQLdatabase::getContactList, static_cast<void *>(&contacts), &error);
+        if(rc != SQLITE_OK ) {
+            fprintf(stderr, "SQL error: %s\n", error);
+        }
+        sqlite3_free(error);
+        query.setData(contacts);
+        std::cout << contacts[0].getName() << ", " << contacts[0].getPort() << ", " << contacts[0].getIp() << std::endl;
+    }
+    std::string serialised(TcpSerializeQuery(query));
+    return serialised;
 }
